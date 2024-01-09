0f5c32c4a6c225a8c0dd64636d2fbafe7934a111
Lehelac <lehelac@gmail.com>
2024-01-09 16:43:08 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const keys = document.getElementsByTagName("li");
+const result = document.querySelector("p");
+const clear = document.querySelector(".clear");
+const ac = document.querySelector(".ac");
+const divis = document.querySelector(".divis");
+const perc = document.querySelector(".percent");
+
+
+for (let i = 0; i < keys.length; i++) {
+    if (keys[i].innerHTML === "=") {
+        keys[i].addEventListener("click", calculate);
+      } else {
+        keys[i].addEventListener("click", addToCurrentValue(i));
+      }
+    }
+
+
+    // function DivCurrentValue (){
+
+    //     if (perc.innerHTML === "%")
+    //     
+
+    //     result.innerHTML/100
+    // }}
+
+
+    function addToCurrentValue(i) {
+        return () => {
+          if (keys[i].innerHTML === "÷") {
+            result.innerHTML += "/";
+          } else if (keys[i].innerHTML === "×") {
+            result.innerHTML += "*";
+          } else {
+            result.innerHTML += keys[i].innerHTML;
+          }
+        };
+      }
+
+      
+      function calculateD () {
+        return (result.innerHTML = (result.innerHTML/100));
+
+      }
+
+
+      function calculate() {
+        return (result.innerHTML = eval(result.innerHTML));
+      }
+    
+      clear.addEventListener("click", () => {
+        result.innerHTML = "";})
+
+        ac.addEventListener("click", () => {
+            result.innerHTML = "";})
+
+     divis.addEventListener("click", () => {
+                result.innerHTML += "/"
+            
+      });
+
+      perc.addEventListener("click", calculateD);
+
+
+    
+      
+    
